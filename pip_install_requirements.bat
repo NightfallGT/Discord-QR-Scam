a9e3cb808e8276e8dfadbab48cb19f8d4a3751dc
Space <unclearspace@gmail.com>
2021-12-10 18:53:48 +0000

    Use a virtualenv when installing modules

--- pip_install_requirements.bat
+++ pip_install_requirements.bat
@@ -1,3 +1,5 @@
 title PIP Install Requirements
-pip install -r requirements.txt
+python -m venv .venv
+.venv\scripts\python.exe -m pip install --upgrade pip
+.venv\scripts\pip.exe install -r requirements.txt
 pause
