9272559a2c02384bed6eac278fcda2cece83be63
Space <unclearspace@gmail.com>
2021-12-11 18:43:42 +0000

    Fix token grabbing

---
+++ grab_token.js
@@ -0,0 +1,2 @@
+// Found on Stack Overflow
+return (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken();
